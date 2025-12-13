514bbc4e045a71e0ffc89d92477ee96063355e88
asqarbekolimov <mr.coderx.pro@gmail.com>
2025-12-13 14:56:07 +0500

    fix: correct script path for products.js and add product rendering functionality

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
     host: "127.0.0.1",
     port: 3000,
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        products: "vanillajs/products.js",
+      },
+    },
+  },
 });
